2efb5d508dfddab8bf9880b296ac0c7e751347bf
Gustavo <110946102+GustavoRez@users.noreply.github.com>
2024-10-04 09:11:32 -0300

    Delete Game Hub directory

--- Game Hub/src/css/css.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const stylesL = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
